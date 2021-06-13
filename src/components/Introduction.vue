6763536442b23bdd0208d9cf26c49478c4773b1e
ExequielGonzalez <gonzalez-exequiel@hotmail.com>
2021-06-13 19:48:58 -0300

    first commit

---
+++ src/components/Introduction.vue
@@ -0,0 +1,106 @@
+<template>
+  <section class="introduction grid">
+    <p class="introduction__brief">
+      Hola, yo soy
+      <span class="introduction__brief-name">Exequiel Gonzalez</span>
+      Web developer
+    </p>
+    <picture>
+      <source
+        class="introduction__photo"
+        media="(min-width:600px)"
+        srcset="../assets/foto2.png"
+        alt="Foto de Exequiel Gonzalez"
+      />
+
+      <img class="introduction__photo" src="../assets/foto.png" alt="Foto de Exequiel Gonzalez" />
+    </picture>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" >
+.introduction {
+  align-items: center;
+  height: 100vh;
+  //   z-index: 1;
+  @media (max-width: $s) {
+    height: 90vh;
+  }
+  &__brief {
+    grid-column: 1/5;
+    color: $accent;
+    font-size: 4rem;
+    line-height: 6rem;
+    @media (max-width: $l) {
+      grid-column: 1/6;
+      font-size: 3rem;
+      line-height: 5rem;
+    }
+    @media (max-width: $m) {
+      grid-column: 1/3;
+      font-size: 2.5rem;
+      line-height: 4rem;
+    }
+
+    @media (max-width: $s) {
+      grid-column: 1/4;
+      font-size: 1.8rem;
+      line-height: 4rem;
+    }
+
+    &-name {
+      display: block;
+      color: $text-color;
+      font-size: 10rem;
+      line-height: 8rem;
+      @media (max-width: $l) {
+        grid-column: 1/2;
+        font-size: 8rem;
+        line-height: 6rem;
+      }
+      @media (max-width: $m) {
+        grid-column: 1/2;
+        font-size: 6rem;
+        line-height: 4rem;
+      }
+      @media (max-width: $s) {
+        grid-column: 1/4;
+        font-size: 4rem;
+        line-height: 4rem;
+      }
+    }
+  }
+  &__photo {
+    grid-column: 6/12;
+    // z-index: 1;
+    width: 40vw;
+    // @media (max-width: $l) {
+    //   width: 650px;
+    // }
+    // @media (max-width: $m) {
+    //   width: 500px;
+    // }
+    @media (max-width: $m) {
+      width: 280px;
+      //   width: 50vw;
+      position: absolute;
+      left: 60%;
+      margin-left: -90px;
+      bottom: 50%;
+      margin-bottom: -170px;
+    }
+    @media (max-width: $s) {
+      width: 210px;
+      position: absolute;
+      left: 50%;
+      margin-left: -55px;
+      bottom: 50%;
+      margin-bottom: -140px;
+    }
+  }
+}
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="navbar grid">
+        <h1 class="navbar__title">Exequiel Gonzalez</h1>
+
+        <q-tabs
+          class="navbar__tab"
+          v-model="tab"
+          indicator-color="transparent"
+          active-color="accent"
+          align="justify"
+          :breakpoint="0"
+        >
+          <q-tab class="navbar__tab-init" name="tab1" label="Inicio" />
+          <q-tab name="tab2" label="Acerca de mi" />
+          <q-tab name="tab3" label="Experiencia" />
+          <q-tab class="navbar__tab-contact" name="tab4" label="Contacto" />
+        </q-tabs>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+
+  data() {
+    return {
+      tab: ""
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  margin-top: 20px;
+  @media (max-width: $m) {
+    margin-top: 0px;
+  }
+  &__title {
+    grid-column: 1/5;
+    font-size: 1.7rem;
+    font-weight: 500;
+    font-weight: 300;
+    font-style: bold;
+    line-height: 1.71rem;
+    // font-family: "Roboto";
+
+    @media (max-width: $m) {
+      // grid-column: 0/0;
+      display: none;
+      // font-size: 1rem;
+    }
+  }
+  &__tab {
+    grid-column: 5/12;
+    @media (max-width: $m) {
+      margin-top: 0px;
+      // display: inline-block;
+      grid-column: 1/5;
+      // display: none;
+    }
+    @media (max-width: $s) {
+      // display: inline-block;
+      // grid-column: 1/4;
+      display: none;
+    }
+    // &-contact {
+    //   color: $accent;
+    //   border: 1px solid $accent;
+    //   border-radius: 10px;
+    // }
+  }
+}
+</style>
